92d0f505371a2b982bdf4ceb4f077d13fee4992e
jswj30 <jswj30@naver.com>
2021-01-15 17:39:56 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
-import MyComponent from "./MyComponent";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import signin from "./signin";
+import mypage from "./mypage";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogin: false,
+    };
+  }
+
   render() {
+    const { isLogin } = this.state;
     return (
-      <MyComponent name="React" favoriteNumber={1}>
-        리액트
-      </MyComponent>
+      <div>
+        <Switch>
+          <Route
+            exact={true}
+            path="/"
+            render={() => {
+              if (isLogin) {
+                return <Redirect to="/mypage" />;
+              }
+              return <Redirect to="/signin" />;
+            }}
+          />
+          <Route path="/signin" component={signin} />
+          <Route path="/mypage" component={mypage} />
+        </Switch>
+      </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/signin.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Signin</h1>
+      </div>
+    );
+  }
+}
+
+export default signin;
