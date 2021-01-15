9a2ca615bc99a4813eb53d68d572b30ce4df9ee7
jswj30 <jswj30@naver.com>
2021-01-15 14:42:31 +0900

    1

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 0,
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Counter;
